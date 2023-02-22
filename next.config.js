dfafcd566e7089a5bac1454578c3e903d5838871
Anupama Jedi <75876536+anupama003@users.noreply.github.com>
2023-02-22 18:38:51 +0530

    deploy modifications

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  
+
+}
+
+module.exports = {
+  extends: [
+    //...
+    'plugin:@next/next/recommended',
+  ],
 }
